2db55f6679e2dfcb8f129b0d375c467be9d92d68
jangsori7021 <jangsori7021@gmail.com>
2026-08-26 02:37:48 +0900

    Add PWA service worker

---
+++ backend/static/sw.js
@@ -0,0 +1,9 @@
+const CACHE='senior-ai-safecheck-v291';
+const CORE=['/','/manifest.json','/icon.svg'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',event=>{
+  const req=event.request;
+  if(req.method!=='GET') return;
+  event.respondWith(fetch(req).then(res=>{const copy=res.clone();caches.open(CACHE).then(cache=>cache.put(req,copy));return res}).catch(()=>caches.match(req).then(hit=>hit||caches.match('/'))));
+});
